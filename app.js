1a950135da7b0009a890b9aef0c1d45231b88c80
JakeDerulo <91212410+JakeDerulo@users.noreply.github.com>
2022-12-02 11:36:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-
-
 const API_KEY = "&apikey=c09cccb2";
 const url="https://omdbapi.com/?s=";
-
 const buttonEl=document.querySelector('#search');
 const inputEl=document.querySelector('#userInput');
 const searchEl=document.querySelector('#movies');
-
 function movieSection(movies) {
     return movies.map((movie) => {
         return `   
@@ -15,15 +11,10 @@ function movieSection(movies) {
         `;
     })
 }
-
 function createMovie(movies){
     const movieElement=document.createElement("div");
-
     movieElement.setAttribute('class', 'movie');
-
-
     img_list = movieSection(movies);
-
     const movieTemplate = 
     `<section class="section">
     ${img_list.join("")}  
@@ -33,13 +24,9 @@ function createMovie(movies){
     <p id="content-close>X</p>
     </div>
     `;
-
-
  movieElement.innerHTML = movieTemplate;
  return movieElement;
-
 }
-
 
 function SearchMovies(data) {
 const movies = data.Search; 
@@ -47,33 +34,23 @@ const movieBlock = createMovie(movies);
 searchEl.appendChild(movieBlock);
 console.log("Data:", data);
 }
-
-
-buttonEl.onclick=function(event) {
-   
-    event.preventDefault();
-    
+buttonEl.onclick=function(event) {  
+    event.preventDefault();  
     const value = inputEl.value;
-
    if (inputEl.value == "") {
     alert("Et voi etsiä elokuvaa jolla ei ole nimeä.");
-   }
-    
+   } 
     const newUrl = url + value + API_KEY;
-
-   
     fetch(newUrl)
     .then((res) => res.json()) 
     .then((data) =>{
         const movies = data.Search; 
         const movieBlock = createMovie(movies);
         searchEl.appendChild(movieBlock);
-        console.log("Data:", data);
-        
+        console.log("Data:", data);    
     })
     .catch((error) =>{ 
         console.log("Error:", error);
     }); 
-
     inputEl.value = ""; 
 }
